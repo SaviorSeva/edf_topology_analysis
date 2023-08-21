c308068fd7884ae999fb31d4fceaee126f60bcfc
SaviorSeva <49302415+SaviorSeva@users.noreply.github.com>
2023-08-21 23:15:41 +0200

    Update analyse.py

--- analyse.py
+++ analyse.py
@@ -7,17 +7,16 @@ class Link:
         self.link_quality = link_quality
         self.forwardLqi = forward_lqi
 
-
 class Node:
-    def __init__(self, id, mac_addr):
-        self.id = id
-        self.mac_addr = mac_addr
+    def __init__(self, id):
+        if id == 0:
+            self.id = -1
+        else:
+            self.id = id
+        self.mac_addr = "-1"
+        self.hop_from_main = -1
+        self.success_rate = 0.0
         self.links = []
-    
-    def __init__(self, id, mac_addr, links):
-        self.id = id
-        self.mac_addr = mac_addr
-        self.links = links
     
     def __str__(self):
         return self.id
@@ -28,29 +27,57 @@ class Node:
 
 
 def main():
-    node_list = []
-
-    with open('voisins-c/voisins-c.log') as voisins_c_file:
-        for voisins_c_line in voisins_c_file:
-
-            vc_data = json.loads(voisins_c_line)
-
-            for neighbours in vc_data['neighbours']:
-                if "shortMac" in neighbours:
-                    if "macEtendue" in neighbours:
-                        node = Node(neighbours['shortMac'], neighbours['macEtendue'])
-                        node_list[neighbours['shortMac']] = node
+    # Get the total number of nodes    
+    node_count = 0
+    with open('traces-cpl/traces-cpl.log') as traces_cpl_file:
+        for traces_cpl_line in traces_cpl_file:
+            tc_data = json.loads(traces_cpl_line)
+            if "shortMac" in tc_data:
+                if tc_data['shortMac'] > node_count:
+                    node_count = tc_data['shortMac']
     
+    node_list = []
+    for i in range (0, node_count+1):
+        node_list.append(Node(i))
+    
+    
+    # Get the mac address of each node
+    with open('traces-cpl/traces-cpl.log') as traces_cpl_file:
+        for traces_cpl_line in traces_cpl_file:
+            tc_data = json.loads(traces_cpl_line)
+
+            if "mac" in tc_data:
+                if "shortMac" in tc_data:
+                    node_list[tc_data['shortMac']].id = tc_data['shortMac']
+                    node_list[tc_data['shortMac']].mac_addr = tc_data['mac']
+
+    # Get the distance to coordinator 
+
+    with open('indicateurs-adp-k/indicateurs-adp-k.log') as indicateurs_adp_k_file:
+            iak_data = json.load(indicateurs_adp_k_file)
+            for entry in iak_data['AdpRoutingTable']:
+                node_list[int(entry['DestAddress'])].hop_from_main = int(entry['HopCount'])
+
+    # Get the droprate (from main coordinator) for each node
+    with open('stats-cpl-k/stats-cpl-k.log') as stats_cpl_k_file:
+        for stats_cpl_k_line in stats_cpl_k_file:
+            sck_data = json.loads(stats_cpl_k_line)
+            found = False
+            for node in node_list:
+                if found: break
+                if node.mac_addr == sck_data['MacEtendue']:
+                    found = True
+                    node.success_rate = int(sck_data['ComOkNumber']) / int(sck_data['ComNumber'])  
+
+    # Get the neighbour data of each node
+
+    i = 0
     for node in node_list:
-        print("Node id: {}, macAddr: {}".format(node.id, node.mac_addr))
+        print("i = {}; Node id: {}, macAddr: {}, hop_to_main: {}, success_rate: {}".format(i, node.id, node.mac_addr, node.hop_from_main, node.success_rate))
+        i = i + 1
 
 
-    # with open('stats-cpl-k/stats-cpl-k.log') as stats_cpl_k_file:
-    #     for stats_cpl_k_line in stats_cpl_k_file:
 
-    #         data = json.loads(stats_cpl_k_line)
-
-    #         print(data['MacEtendue'])
 
 if __name__ == "__main__":
     main()
