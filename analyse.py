19b80bf2ac3d2de86d6756a7d6021b6ad195ddcf
SaviorSeva <49302415+SaviorSeva@users.noreply.github.com>
2023-09-01 08:35:07 +0200

    Update analyse.py

--- analyse.py
+++ analyse.py
@@ -1,8 +1,25 @@
 import json
+import xml.etree.ElementTree as etree
 
 # According to Enedis, the average SNR value (in dB) equals to (LQIvalue / 4) - 10
 def calc_SNR(lqi_value):
     return (lqi_value / 4) - 10
+
+def to_eui(id):
+    if id < 15 :
+        return "FF:FF:FF:FF:FE:00:00:0" + format(id+1, 'x')
+    elif id < 255:
+        return "FF:FF:FF:FF:FE:00:00:" + format(id+1, 'x')
+    else :
+        return None
+    
+
+# SNR(dB) to Attenuation(dB) when noise=48dBuV: Att = 72-SNR, SNR range [-10, 22]
+# LQI to Attenuation(dB) when noise=48dBuV: Att = 82-0.25*LQI, LQI range [0, 128]
+# Attuation range: [50, 82]
+def lqi_to_att(lqi_value):
+    return int(82 - 0.25*int(lqi_value))
+
 
 # A class for storing link info.
 # pvt: pos table valid time, used for choosing a valid link_quality value from multiple entries
@@ -55,10 +72,11 @@ def main():
             tc_data = json.loads(traces_cpl_line)
             if "shortMac" in tc_data and tc_data['shortMac'] > node_count:
                 node_count = tc_data['shortMac']
-    
+    node_count = node_count + 1
+
     # Create a list of nodes for further use
     node_list = []
-    for i in range (0, node_count+1):
+    for i in range (0, node_count):
         node_list.append(Node(i))
     
     # Get the mac address of each node
@@ -141,24 +159,24 @@ def main():
                             link = Link(0, node.id, vk_data['LQIStatistics']['AverageRevLQI'], -2)
                             node_list[0].addLink(link)
 
-    # Calculate the success rate for each link 
+    # Calculate the root = gfg.Element("Catalog")success rate for each link 
     # First, get the max hop count
     max_hop_count = -1
     current_count = 2
-    for i in range (1, node_count+1):
+    for i in range (1, node_count):
         if node_list[i].hop_from_main > max_hop_count:
             max_hop_count = node_list[i].hop_from_main
 
     # Easy one: the nodes with 1 hop count: set the link success rate by the node success rate directly
-    for i in range (1, node_count+1):
+    for i in range (1, node_count):
         if node_list[i].hop_from_main == 1:
            node_list[0].links[node_list[0].indexOfLink(0, i)].setSuccessRate(node_list[i].success_rate)
 
     # Then for node with hop_count h, msg will pass a node that has h-1 hop count, thus calculate the success rate of link
     while current_count <= max_hop_count:
-        for i in range (1, node_count+1):
+        for i in range (1, node_count):
             if node_list[i].hop_from_main == current_count:
-                for m in range (1, node_count+1):
+                for m in range (1, node_count):
                     if node_list[m].hop_from_main == current_count-1:
                         link_success_rate = node_list[i].success_rate / node_list[m].success_rate
                         if link_success_rate <= 1.0:
@@ -169,16 +187,102 @@ def main():
             
     # Print all infos to the terminal. 
     # To show all infos please redirect the output to a file.
-    i = 0
-    for node in node_list:
-        l = 1
-        print("Node {}:\nmacAddr: {}, hop_to_main: {}, success_rate: {}\nNeighbours:".format(node.id, node.mac_addr, node.hop_from_main, node.success_rate))
-        for link in node.links:
-            print("Link {}:".format(l), link.toString())
-            l = l + 1
+    # i = 0
+    # for node in node_list:
+    #     l = 1
+    #     print(f"Node {node.id}:\nmacAddr: {node.mac_addr}, hop_to_main: {node.hop_from_main}, success_rate: {node.success_rate}\nNeighbours:")
+    #     for link in node.links:
+    #         print("Link {}:".format(l), link.toString())
+    #         l = l + 1
 
-        print()
-        i = i + 1
+    #     print()
+    #     i = i + 1
+
+    print(f"Node count: {node_count}")
+
+    # Translate the info from node_list to nSim xml config file
+    sim_config = etree.Element("simulation_config")
+    
+    sim_config.set('xmlns:xsi', "http://www.w3.org/2001/XMLSchema-instance")
+    sim_config.set('xsi:noNamespaceSchemaLocation', "../../../share/xml/schema/nsim/g3_config.xsd")
+
+    simu = etree.Element("simulator")
+    sim_config.append(simu)
+
+    nodes = etree.SubElement(simu, "nodes")
+    nodes.text = str(node_count)
+
+    target = etree.SubElement(simu, "target")
+    target.text = "lib/x64/libg3_target.so"
+
+    sp_target = etree.SubElement(simu, "special_target")
+    sp_target.set('eui', to_eui(0))
+    sp_target.text = "lib/x64/libg3_coord_target.so"
+
+    min_time_step = etree.SubElement(simu, "min_time_step")
+    min_time_step.text = "100000"
+
+    max_time_step = etree.SubElement(simu, "max_time_step")
+    max_time_step.text = "500000000"
+
+    # node_configuration
+    node_config = etree.Element("node_configuration")
+    sim_config.append(node_config)
+    defaults = etree.Element("defaults")
+    node_config.append(defaults)
+
+    g3app = etree.Element("g3app")
+    defaults.append(g3app)
+    pan_id = etree.Element("pan_id")
+    pan_id.text = "23620"
+    g3app.append(pan_id)
+
+
+    g3tstapp = etree.Element("g3tstapp")
+    defaults.append(g3tstapp)
+    mode = etree.Element("mode")
+    mode.text = "0"
+    g3tstapp.append(mode)
+
+    # connectivity
+    connectivity = etree.Element("connectivity")
+    sim_config.append(connectivity)
+    defaults = etree.Element("defaults")
+    connectivity.append(defaults)
+    droprate = etree.Element("droprate")
+    droprate.text = "100"
+    defaults.append(droprate)
+    g3_noise = etree.Element("g3_noise")
+    g3_noise.text = "48"
+    defaults.append(g3_noise)
+    g3_maxlqi = etree.Element("g3_maxlqi")
+    g3_maxlqi.text = "128"
+    defaults.append(g3_maxlqi)
+
+    for node in node_list:
+        node_xml = etree.Element("node")
+        node_xml.set('eui', to_eui(node.id))
+        connectivity.append(node_xml)
+
+        for link in node.links:
+            peer = etree.Element("peer")
+            peer.set('eui', to_eui(link.id_end))
+            node_xml.append(peer)
+            droprate = etree.Element("droprate")
+            droprate.text = "0"
+            peer.append(droprate)
+
+            g3_attenuation = etree.Element("g3_attenuation")
+            g3_attenuation.text = str(lqi_to_att(link.link_quality))
+            peer.append(g3_attenuation)
+
+
+    tree = etree.ElementTree(sim_config)
+    etree.indent(tree, '  ')
+
+    with open ("topo.xml", "wb") as output :
+        tree.write(output, encoding='utf-8', xml_declaration=True)
+
 
     # This following code verifies no duplicated link exists for every node
     # i = 0
